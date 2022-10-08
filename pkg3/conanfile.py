f218661e68e0246db523518d8ca4252dabe29da9
Alex Trotta <alext@glodonusa.com>
2022-10-08 00:32:54 -0400

    Simplify example to only 3 packages

--- pkg3/conanfile.py
+++ pkg3/conanfile.py
@@ -4,4 +4,8 @@ class Pkg3Conan(ConanFile):
     name = "pkg3"
 
     def requirements(self):
-        self.requires("pkg2/1.0.0@build-req-repro/test")
+        self.requires("pkg1/1.1.0@build-req-repro/test")
+        
+    def build_requirements(self):
+        self.test_requires("pkg2/1.0.0@build-req-repro/test")
+        self.test_requires("pkg1/1.1.0@build-req-repro/test")
